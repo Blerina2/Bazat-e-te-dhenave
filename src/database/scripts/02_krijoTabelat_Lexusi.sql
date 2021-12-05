8a7465ffabce1953e8639f29331e9d8b6394c80b
Blerina2 <blerina.beka@student.uni-pr.edu>
2021-12-05 20:38:40 +0100

    Krijimi i Data Bazes Projekti

---
+++ src/database/scripts/02_krijoTabelat_Lexusi.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Lexusi(
+    ID int auto_increment primary key,
+    Emri varchar(255),
+    Mbiemri varchar(255),
+    EmriPrindit varchar(255),
+    Gjinia varchar(255),
+    Datelindja DATE #format YYYY-MM-DD
+
+)
