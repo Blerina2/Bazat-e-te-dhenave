d01f611d9ccc3c5a74a74d09e09545efafe65a41
Arbreshass <93888146+Arbreshass@users.noreply.github.com>
2021-12-27 22:02:20 +0100

    Krijimi I sql Query 4-7

--- src/database/faza 2/sqlQuery1deri9.sql
+++ src/database/faza 2/sqlQuery1deri9.sql
@@ -8,3 +8,23 @@ from lexuesi l inner join huazimi h on l.ID = h.LexuesiId where h.DataeJepjes>='
 select *
 from lexuesi l  inner join huazimi h on l.ID = h.LexuesiId
 where DATE_ADD(h.Kthimi, INTERVAL 15 DAY) and (h.DataeJepjes>='2020-01-01' and h.DataeJepjes<='2021-12-31');
+# 4. Listoni ID-të dhe titullin e librave qe ne vitin 2019 ose 2020 jane huazur se paku nje here kurse ne vitin 2021 asnjehere .
+select *
+from libri l inner join huazimi h on l.ID = h.LibriId
+where (h.DataeJepjes>='2019-01-01' and h.DataeJepjes<='2020-12-31')
+and  !(h.DataeJepjes>='2021-01-01' and h.DataeJepjes<='2021-12-31');
+# 5.Listoni top 5 lexuesit me numrin me te madh të huazimeve në 3 muajt e fundit.
+#Lista të paraqes të dhënat e lexusit duke përfshirë emrin e mbiemrin si dhe numrin e huazimeve që ka realizuar.
+select l.emri, l.mbiemri,
+from lexuesi l inner join huazimi h on l.ID = h.LexuesiId where DATE_ADD(h.DataeJepjes, INTERVAL 90 DAY) LIMIT 5 ;
+# 6.Gjeni shumen e fituar nga te gjitha anetaresimet, te ndare ne vite 2019,2020,2021. Rezultati duhet te jete nje tabele te ka fushat Viti,Shuma.
+select SUM(l.qmimi) as shuma,l.DataeJepjes as shuma
+from libri l where (l.DataeJepjes>='2019-01-01' and l.DataeJepjes<='2021-12-31');
+# 7. Për secilin lexues paraqitni (në një listë të vetme), per vitin aktual:
+# Emri, mbiemri,
+# numrin e librave qe ka huazuar,
+# zhanri i preferuar,
+# sa here e ka vonuar librin(nese ska vonesa, numri eshte 0)
+select emri , mbiemri , huazim ,zhanri
+from ((bibloteka b inner join lexuesi l on l.ID=b.LexuesiId) inner join libri li on li.Id=b.libriId)
+inner join huazimi h  on h.lexuesId=l.Id where h.vonesa>0;
