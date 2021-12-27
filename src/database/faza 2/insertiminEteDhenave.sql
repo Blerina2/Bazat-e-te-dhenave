233248994476033b94a09bde616f415bdd71e371
Albulena <albulena.makolli2@student.uni-pr.edu>
2021-12-27 10:48:35 -0800

    Insertimi i te dhenave te tabelave Libri, Puntori, Paga dhe LibratDemtuar

--- src/database/faza 2/insertiminEteDhenave.sql
+++ src/database/faza 2/insertiminEteDhenave.sql
@@ -39,5 +39,73 @@ VALUES(2,'2019-10-01','2019-10-01',2,'Salhi',5,'2021-12-10') ;
 insert into detajetlexuesi ( LexuesiId, VitiIRegjistrimit, DataRegjestrimit, CmimiIRegjistrimit, PuntoriIRegjistruar, Antarsimi, Kushtezimi)
 VALUES(1,'2021-11-11','2021-11-11',1,'Baci',1,'2028-12-10') ;
 
+# Insertimi i Tabeles se Detajet e pages
+
+insert into paga (Profesor, Student, Nxenes)
+VALUES('Arsimtar','Student','Nxenes');
+insert into paga (Profesor, Student, Nxenes)
+VALUES('Doktor','Student','Nxenes');
+insert into paga (Profesor, Student, Nxenes)
+VALUES('Inxhinier','Student','Nxenes');
+insert into paga (Profesor, Student, Nxenes)
+VALUES('Artist','Student','Nxenes');
+insert into paga (Profesor, Student, Nxenes)
+VALUES('Pilot','Student','Nxenes');
+
+# Insertimi i Tabeles se librave te demtuar
+
+insert into libratDemtuar (LibriId, GjendjaELibrit, Vrejtje, DataRegjistrimi, PuntoriID)
+VALUES(1,0,'Libri eshte i demtuar','2008-04-12',1);
+insert into libratDemtuar (LibriId, GjendjaELibrit, Vrejtje, DataRegjistrimi, PuntoriID)
+VALUES(2,1,'Libri nuk e ka nje faqe','2006-05-15',2);
+insert into libratDemtuar (LibriId, GjendjaELibrit, Vrejtje, DataRegjistrimi, PuntoriID)
+VALUES(3,2,'Libri eshte i demtuar','2020-01-15',3);
+insert into libratDemtuar (LibriId, GjendjaELibrit, Vrejtje, DataRegjistrimi, PuntoriID)
+VALUES(4,3,'Libri nuk i ka disa faqe','2021-06-10',4);
+insert into libratDemtuar (LibriId, GjendjaELibrit, Vrejtje, DataRegjistrimi, PuntoriID)
+VALUES(5,4,'Libri eshte i demtuar','2019-02-12',5);
+
+# Insertimi i Tabeles puntor
+
+insert into puntor (Emri, Mbiemri, AdresaId, TelefoniId, Email, PagaId)
+VALUES('Vlera', 'Lumi', 1, 1, 'Vleralumi2@gmail.com', 1);
+insert into puntor (Emri, Mbiemri, AdresaId, TelefoniId, Email, PagaId)
+VALUES('Noela', 'Kastrati', 2, 2, 'Noelakastrati1@gmail.com', 2);
+insert into puntor (Emri, Mbiemri, AdresaId, TelefoniId, Email, PagaId)
+VALUES('Elmedina', 'Gashi', 3, 3, 'Elmedina.gashi@gmail.com', 3);
+insert into puntor (Emri, Mbiemri, AdresaId, TelefoniId, Email, PagaId)
+VALUES('Eriola', 'Rexha', 4, 4, 'Eriolarexha4@gmail.com', 4);
+insert into puntor (Emri, Mbiemri, AdresaId, TelefoniId, Email, PagaId)
+VALUES('Idea', 'Rexhepi', 5, 5, 'Idearexhepi1@gmail.com', 5);
+
+# Insertimi i te dhenave ne Tabelen libri
+
+insert into libri (Titulli, Autori, Zhanri, NumriIKopjeve, DataPranimit, PuntoriId)
+VALUES('Programimi ne C++', 'Blerim Bytyci', 'Mesimor', 1,'2020-02-13',1);
+insert into libri (Titulli, Autori, Zhanri, NumriIKopjeve, DataPranimit, PuntoriId)
+VALUES('Programimi ne JavaScript', 'Naim Ternava', 'Mesimor', 2,'2019-05-19',2);
+insert into libri (Titulli, Autori, Zhanri, NumriIKopjeve, DataPranimit, PuntoriId)
+VALUES('Programimi ne Python', 'Shemsedin Gashi', 'Mesimor', 3,'2020-07-21',3);
+insert into libri (Titulli, Autori, Zhanri, NumriIKopjeve, DataPranimit, PuntoriId)
+VALUES('Programimi ne Html', 'Labinot Rexha', 'Mesimor', 4,'2018-01-25',4);
+insert into libri (Titulli, Autori, Zhanri, NumriIKopjeve, DataPranimit, PuntoriId)
+VALUES('Programimi ne mySql', 'Fisnik Rexhepi', 'Mesimor', 5,'2021-05-10',5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
